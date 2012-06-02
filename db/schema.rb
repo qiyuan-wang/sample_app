c0f21d566a8e2b73655e9c999c78a2ff06d360d4
Qiyuan Wang <zephyrisalive@gmail.com>
2012-06-02 13:05:39 +0800

    a

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120531132959) do
+ActiveRecord::Schema.define(:version => 20120602035708) do
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
